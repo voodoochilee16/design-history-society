91172519f930f4be2525d1b1780346fe925966d2
Paulo Ferreira <>
2022-09-29 16:10:15 +0100

    vercel

--- dhs_test/svelte.config.js
+++ dhs_test/svelte.config.js
@@ -1,6 +1,6 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import adapter from "@sveltejs/adapter-static";
+// import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,13 +9,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(
-			{
-				pages: 'build',
-				assets: 'build',
-				fallback: null
-			}
-		)
+		adapter: adapter()
 	}
 };
 
