76e8af4d30154e5d2f1736cabc0601525e54bb63
Paulo Ferreira <>
2022-09-29 17:39:44 +0100

    deploy live vercel

--- dhs_test/src/lib/components/+nav.svelte
+++ dhs_test/src/lib/components/+nav.svelte
@@ -1,8 +1,15 @@
 <nav>
-    <ul>
+    <ul class="navbar">
         <a href="/">Home Page</a>
         <a href="/team">Team</a>
     </ul>
 </nav>
-
+<style>
+.navbar{
+    display: flex;
+    gap: 30px;
+    font-size: 20px;
+}
+  
+</style>
 <slot></slot>
